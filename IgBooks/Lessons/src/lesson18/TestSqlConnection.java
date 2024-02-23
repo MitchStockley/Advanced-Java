c3b7e98d4c386e02a2052274ab30bd52d34eee2f
Mitchell <mitchell.stockley@gmail.com>
2024-02-23 09:38:40 +0200

    updated code

--- IgBooks/Lessons/src/lesson18/TestSqlConnection.java
+++ IgBooks/Lessons/src/lesson18/TestSqlConnection.java
@@ -35,6 +35,7 @@ public class TestSqlConnection {
                     t = t.getCause();
                 }
                 ex = ex.getNextException();
+                //
             }
         }
     }
